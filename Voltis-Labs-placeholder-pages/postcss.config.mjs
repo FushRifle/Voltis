fa33c84b14e0aa55c384747f45f2feaa88e16667
Abdullahi Olawale Mukaila <ayomikail21@gmail.com>
2025-03-29 17:26:18 +0100

    Delete Voltis-Labs-placeholder-pages directory

--- Voltis-Labs-placeholder-pages/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},  // Ensures cross-browser compatibility
-  },
-};
-
-export default config;
